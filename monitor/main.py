d24d825bd96b697cdd626582be64098c2ba57c0a
JFellipeLima <felipelimaferreira21@gmail.com>
2026-03-29 17:38:27 -0300

    refactor: improve structure and fix naming

--- monitor/main.py
+++ monitor/main.py
@@ -1,33 +1,35 @@
 #!/bin/python3
+import asyncio
 import httpx
-import asyncio 
 
 #Funçôes
-async def Save_data(response):
+async def save_data(response):
     '''Save the log information in database'''
-    body = response
-    print(body)
+    print(response)
 
 
-async def Verify(HOST, HEADERS):
-    '''Connecr and get informarion with host'''
-    async with httpx.AsyncClient(headers=HEADERS,http2=True, follow_redirects=True,timeout=10) as client:
+async def verify(domain, header):
+    '''Connect and get information with host'''
+    async with httpx.AsyncClient(
+            headers=header,
+            http2=True,
+            follow_redirects=True,
+            timeout=10
+            ) as client:
         while True:
-            response = await client.head(HOST)
-            await Save_data(response.status_code)
+            response = await client.head(domain)
+            await save_data(response.status_code)
             await asyncio.sleep(15)
 
-        return "Connection time expired"
 
-
-#Testes
+#Tests
 if __name__ == "__main__":
-    HEADERS= {
+    header = {
             "Accept-Encoding": "gzip, deflate",
             "accept": "application/json"
             }
-    HOST= "https://bancocn.com"
+    domain = "https://google.com"
     try:
-        asyncio.run(Verify(HOST, HEADERS))
+        asyncio.run(verify(domain, header))
     except KeyboardInterrupt:
-        print("Manualy interrompted")
+        print("Manually interrupt")
