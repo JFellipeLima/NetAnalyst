f3ecdf4dca0848ae254b206350baf44eb866a387
JFellipeLima <felipelimaferreira21@gmail.com>
2026-04-11 23:33:39 -0300

    fix: set the correct sleep time for analytic

--- monitor/databasse/data.py
+++ monitor/databasse/data.py
@@ -1,19 +1,25 @@
 """Work in analise of logs and save information in database"""
 
 from datetime import datetime, UTC, timedelta
-#from err_save import log_error
+
+# from err_save import log_error
+
 
 async def save_data(db, url):
     """Analyse logs and save analytics"""
     db_collection = db["analytics"]
     db_log = db["logs"]
 
-    last_hour = datetime.now(UTC) - timedelta(minutes=3)
+    last_hour = datetime.now(UTC) - timedelta(hours=1)
     check_last = await db_collection.find_one({"domain_name": url}, sort=[("date", -1)])
     check_last_update = check_last["date"].replace(tzinfo=UTC) if check_last else None
 
-    if check_last_update is None or datetime.now(UTC) - check_last_update >= timedelta(minutes=3):
-        stats = await db_log.find({"date": {"$gte": last_hour}, "domain": url}).to_list(None)
+    if check_last_update is None or datetime.now(UTC) - check_last_update >= timedelta(
+        hours=1
+    ):
+        stats = await db_log.find({"date": {"$gte": last_hour}, "domain": url}).to_list(
+            None
+        )
         latencies = sorted([i["latency_ms"] for i in stats])
 
         if not stats:
@@ -46,7 +52,7 @@ async def save_data(db, url):
             "avg_latency": avg_latency,
             "incidents": incidents,
             "status": status,
-            "date": datetime.now(UTC)
+            "date": datetime.now(UTC),
         }
         await db_collection.insert_one(body)
         print(f"Saved analytics: \n{body}\n")
