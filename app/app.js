d7b4aef85cb9752d32a55bdba13772a291f73a55
JFellipeLima <felipelimaferreira21@gmail.com>
2026-04-13 23:42:06 -0300

    add: static arquives

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 import dotenv from "dotenv"
+import path from "path"
 import express from "express"
 import cors from "cors"
 import connect_db from "./schemas/database.js"
 import routes from "./routes/logRoutes.js"
 
 const app = express()
-dotenv.config()
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+dotenv.config(__dirname + "../.env")
 
 app.use(express.json())
 app.use(cors({
@@ -15,6 +18,7 @@ app.use(cors({
 }))
 app.use(express.urlencoded({ extended: true }))
 app.use("/", routes)
+app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(process.env.PORT, () => {
   connect_db(process.env.MONGO_NODE_URL)
