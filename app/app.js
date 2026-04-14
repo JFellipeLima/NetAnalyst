b65b9eff5e7a3a4b71ace666637254f70324ed8e
JFellipeLima <felipelimaferreira21@gmail.com>
2026-04-14 09:32:40 -0300

    add: CORS and dotenv configs

--- app/app.js
+++ app/app.js
@@ -1,15 +1,19 @@
 import dotenv from "dotenv"
 import express from "express"
+import path from "path"
 import cors from "cors"
 import connect_db from "./schemas/database.js"
 import routes from "./routes/logRoutes.js"
 
 const app = express()
-dotenv.config()
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+dotenv.config({ path: path.resolve(__dirname + "/../.env") })
+console.log(__dirname + ".env")
 
 app.use(express.json())
 app.use(cors({
-  origin: "*",
+  origin: process.env.CORS_ORIGIN,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
